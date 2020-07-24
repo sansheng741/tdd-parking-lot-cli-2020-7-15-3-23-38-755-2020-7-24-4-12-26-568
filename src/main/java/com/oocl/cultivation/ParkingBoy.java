2c179c212c993852d578f8740ec471ec5b9cfaa6
sansheng <807156237@qq.com>
2020-07-24 20:52:18 +0800

    Parking Lot [colin] feature : pass should_return_no_ticket_when_parkCar_given_car_and_parkingboy_and_full_ParkingLot

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,6 +1,7 @@
 package com.oocl.cultivation;
 
 
+import java.util.List;
 
 /**
  * @author ck
@@ -15,6 +16,19 @@ public class ParkingBoy {
 
 
     public Ticket parkCar(Car car) {
+        boolean isPark = parkCarInParkingLot(car);
+        if(!isPark){
+            return null;
+        }
         return new Ticket();
+    }
+
+    private boolean parkCarInParkingLot(Car car){
+        List<Car> carList = parkingLot.getCarList();
+        if(carList.size() >= 10){
+            return false;
+        }
+        carList.add(car);
+        return true;
     }
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,11 +1,15 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ck
  * @create 2020-07-24 20:32
  */
 public class ParkingLot {
-    int capacity;
+    private int capacity;
+    private List<Car> carList = new ArrayList<>();
 
     public ParkingLot() {
         this.capacity = 10;
@@ -19,7 +23,7 @@ public class ParkingLot {
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
+    public List<Car> getCarList() {
+        return carList;
     }
 }
